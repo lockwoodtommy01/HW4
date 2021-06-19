72ec4eb3402a623f5a8541670c7ad82734e77841
Your Name <you@example.com>
2021-06-19 13:41:35 -0400

    first commit

---
+++ assets/js/questions.js
@@ -0,0 +1,28 @@
+// questions with multiple choice answers and correct answer
+var questions = [
+    {
+        title: "Alerts do NOT include:",
+        choices: ["parentheses", "semicolons", "curly braces", "quotes"],
+        answer: "curly braces"
+    },
+    {
+        title: "Which of these CAN be a variable name?",
+        choices: ["20", "this", "function", "someThing"],
+        answer: "someThing"
+    },
+    {
+        title: "If unsure about certain syntax, what is the phrase used for google searching the issue?",
+        choices: ["Google Kung", "Google Fu", "Bing", "Internet Intelligence"],
+        answer: "Google Fu"
+    },
+    {
+        title: "What are arrays used for in Javascript?",
+        choices: [
+            "numbers and strings",
+            "other arrays",
+            "booleans",
+            "all of the above"
+          ],
+        answer: "all of the above"
+    }
+];
